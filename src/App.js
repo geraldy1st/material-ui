c951b2aecf9293c9aa22cd4e3c988f3642db74fe
geraldy1st <geraldy.leondas@gmail.com>
2020-12-08 17:54:25 +0100

    using AppBar

--- src/App.js
+++ src/App.js
@@ -11,14 +11,19 @@ import TextField from '@material-ui/core/TextField'
 
 import {makeStyles, ThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 
-import {green, orange } from '@material-ui/core/colors'
-import 'fontsource-roboto';
+import { green, orange } from '@material-ui/core/colors'
+import 'fontsource-roboto'
 
 import Typography from '@material-ui/core/Typography'
 import Container from '@material-ui/core/Container'
 
 import Paper from '@material-ui/core/Paper'
 import Grid from '@material-ui/core/Grid'
+
+import AppBar from '@material-ui/core/AppBar'
+import ToolBar from '@material-ui/core/ToolBar'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
 
 
 const useStyles = makeStyles({
@@ -39,14 +44,14 @@ const theme = createMuiTheme({
             marginBottom: 15
         }
     },
-    palette: {
-        primary: {
-            main: orange[400],
-        },
-        secondary: {
-            main: green[400],
-        }
-    }
+    // palette: {
+    //     primary: {
+    //         main: green[300],
+    //     },
+    //     secondary: {
+    //         main: orange[200],
+    //     }
+    // }
 })
 
 function ButtonStyled(){
@@ -82,7 +87,20 @@ function App() {
       <ThemeProvider theme={theme}>
           <Container maxWidth="xs">
             <div className="App">
-                <header className="App-header">
+                <header className="App-header" style={{paddingTop: 15}}>
+                    <AppBar color='secondary'>
+                        <ToolBar>
+                            <IconButton>
+                                <MenuIcon />
+                            </IconButton>
+                            <Typography variant='h6'>
+                                MUI Themeing
+                            </Typography>
+                            <Button>
+                                Login
+                            </Button>
+                        </ToolBar>
+                    </AppBar>
                     <Typography variant="h2" component="div">
                         Welcome to MUI
                     </Typography>
@@ -91,12 +109,14 @@ function App() {
                     </Typography>
                 <ButtonStyled />
                 <Grid container spacing={4} justify="center">
-                    <Grid item>
-                        <Paper style={{height:75, width: 50}} />
-                    </Grid><Grid item>
-                        <Paper style={{height:75, width: 50}} />
-                    </Grid><Grid item>
-                        <Paper style={{height:75, width: 50}} />
+                    <Grid item xs={3} sm={6} >
+                        <Paper style={{height:75,  width: "100%"}} />
+                    </Grid>
+                    <Grid item xs={3} sm={6}>
+                        <Paper style={{height:75, width: "100%"}} />
+                    </Grid>
+                    <Grid item xs={3} sm={6} lg={12}>
+                        <Paper style={{height:75, width: "100%"}} />
                     </Grid>
                 </Grid>
                 <br/>
